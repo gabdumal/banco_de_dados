42ab51a079f49e5062c88cfe3701a20a2f112f25
Gabriel Malosto <gabriel.malosto@estudante.ufjf.br>
2023-12-12 02:18:20 -0300

    Aprimora queries com JOINs

--- script-popula-tema1.sql
+++ script-popula-tema1.sql
@@ -18,7 +18,11 @@ INSERT INTO passageiro (id_passageiro, nome, sobrenome, endereco, telefone, emai
 (1, 'Igor', 'Amado', 'Rua das Flores, 123, Centro, Americana, São Paulo, Brasil', '+55 11 98765-4321', 'igor@email.com'),
 (2, 'Tiago', 'Santana', 'Avenida Minas Gerais, 456, Centro, Juiz de Fora, Minas Gerais, Brasil', '+55 21 91255-6789', 'tiago@email.com'),
 (3, 'Bruna', 'Campos', 'Avenida Brasil, 1056, Vila Veneza, Extrema, Minas Gerais, Brasil', '+1 212-555-6789', 'bruna@email.com'),
-(4, 'Roberta', 'Coimbra', 'Central Street, 789, Whitechapel, Londres, Inglaterra', '+44 20 1234 5678', 'roberta@email.com');
+(4, 'Roberta', 'Coimbra', 'Central Street, 789, Whitechapel, Londres, Inglaterra', '+44 20 1234 5678', 'roberta@email.com'),
+(5, 'Mariana', 'Silva', 'Rua das Palmeiras, 789, Jardim Botânico, Rio de Janeiro, Rio de Janeiro, Brasil', '+55 21 98765-4321', 'mariana@email.com'),
+(6, 'Carlos', 'Pereira', 'Rua dos Pinheiros, 234, Pinheiros, São Paulo, São Paulo, Brasil', '+55 11 87654-3210', 'carlos@email.com'),
+(7, 'Sophie', 'Dubois', 'Rue de la Paix, 567, Le Marais, Paris, França', '+33 1 23 45 67 89', 'sophie@email.com'),
+(8, 'Alexandre', 'Oliveira', 'Av. da Liberdade, 123, Baixa, Lisboa, Portugal', '+351 21 987 6543', 'alexandre@email.com');
 
 -- Populando tabela aeronave
 INSERT INTO aeronave (id_aeronave, tipo_aeronave, capacidade) VALUES
@@ -41,14 +45,56 @@ INSERT INTO voo (numero_voo, aeroporto_partida, aeroporto_chegada, hora_partida,
 -- Populando tabela bilhete
 INSERT INTO bilhete (numero_bilhete, numero_assento, preco_bilhete, id_passageiro, numero_voo) VALUES
 ('ZXL958212', '15', 2962.00, 2, 'FL08573'),
-('AN743F41P', '20A', 5209.00, 1, 'MNA98756'),
-('B230954', '2', 450.00, 3, 'S8591');
+('ZXL958213', '18', 2500.00, 5, 'FL08573'),
+('ZXL958214', '12', 3200.00, 6, 'FL08573'),
+('ZXL958215', '5', 2900.00, 7, 'FL08573'),
+('B230955', '2', 450.00, 3, 'S8573'),
+('B230956', '22', 4200.00, 1, 'S8573'),
+('B230957', '15', 3500.00, 7, 'S8573'),
+('B230958', '7', 3800.00, 2, 'S8573'),
+('B231034', '2', 900.00, 3, 'S8591'),
+('B231035', '3', 1100.00, 7, 'S8591'),
+('B231036', '4', 750.00, 5, 'S8591'),
+('B231037', '5', 750.00, 2, 'S8591'),
+('UA958B216', '30A', 5000.00, 1, 'FL08923'),
+('UA958B217', '30B', 5000.00, 6, 'FL08923'),
+('UA958B218', '25A', 4800.00, 7, 'FL08923'),
+('UA958B219', '15B', 5200.00, 2, 'FL08923'),
+('AN743F41P', '5A', 7500.00, 1, 'MNA98756'),
+('AN743F42E', '20B', 8000.00, 2, 'MNA98756'),
+('AN743F43E', '8C', 7200.00, 3, 'MNA98756'),
+('AN743F44C', '20A', 5209.00, 6, 'MNA98756'),
+('LS839X7392', '12C', 9500.00, 3, 'A0V09231'),
+('LS839X7393', '7A', 9200.00, 1, 'A0V09231'),
+('LS839X7394', '7B', 9200.00, 5, 'A0V09231'),
+('LS839X7395', '20A', 9800.00, 6, 'A0V09231');
 
 -- Populando tabela reserva
 INSERT INTO reserva (id_reserva, data_reserva, id_passageiro, numero_voo) VALUES
-(1, '2023-11-30 04:09:32', 2, 'FL08573'),
-(2, '2023-12-09 12:46:12', 1, 'MNA98756'),
-(3, '2023-12-06 09:18:08', 3, 'S8591');
+(1, '2023-11-30 09:00:00', 2, 'FL08573'),
+(2, '2023-11-28 15:30:00', 5, 'FL08573'),
+(3, '2023-11-25 11:45:00', 6, 'FL08573'),
+(4, '2023-11-20 14:20:00', 7, 'FL08573'),
+(5, '2023-12-05 08:45:00', 3, 'S8573'),
+(6, '2023-12-02 16:10:00', 1, 'S8573'),
+(7, '2023-11-30 10:30:00', 7, 'S8573'),
+(8, '2023-11-25 09:15:00', 2, 'S8573'),
+(9, '2023-12-05 13:30:00', 3, 'S8591'),
+(10, '2023-12-02 12:15:00', 7, 'S8591'),
+(11, '2023-11-30 08:45:00', 5, 'S8591'),
+(21, '2023-11-28 18:30:00', 2, 'S8591'),
+(12, '2023-12-08 18:00:00', 1, 'FL08923'),
+(13, '2023-12-05 14:30:00', 6, 'FL08923'),
+(14, '2023-12-03 11:45:00', 7, 'FL08923'),
+(22, '2023-12-03 10:15:00', 2, 'FL08923'),
+(15, '2023-12-10 17:30:00', 1, 'MNA98756'),
+(16, '2023-12-08 13:15:00', 2, 'MNA98756'),
+(17, '2023-12-05 10:30:00', 3, 'MNA98756'),
+(23, '2023-12-11 16:45:00', 6, 'MNA98756'),
+(18, '2023-12-12 02:45:00', 3, 'A0V09231'),
+(19, '2023-12-09 08:15:00', 1, 'A0V09231'),
+(20, '2023-12-07 14:30:00', 5, 'A0V09231'),
+(24, '2023-12-14 02:00:00', 6, 'A0V09231');
 
 -- Populando tabela emprego
 INSERT INTO emprego (id_emprego, data_inicio, data_fim, id_funcionario, codigo_aeroporto) VALUES
--- script-queries-inner-join-tema1.sql
+++ script-queries-inner-join-tema1.sql
@@ -1,12 +1,28 @@
--- Query 1: Retorna os detalhes dos voos junto com as informações das aeronaves associadas a esses voos
-SELECT v.*, a.*
-FROM voo v
-INNER JOIN aeronave a ON v.id_aeronave = a.id_aeronave;
+/*
+Query 1: Retorna os detalhes dos voos junto com as informações das aeronaves associadas a esses voos
+Essa query é útil para visualizar informações gerais dos voos. Com a condição de mostrar apenas os voos futuros, é
+possível montar um quadro daqueles que ainda ocorrerão.
+*/
+SELECT vo.numero_voo, vo.aeroporto_partida,  vo.hora_partida, vo.aeroporto_chegada, vo.hora_chegada,
+	vo.tipo_aeronave as tipo_aeronave_esperado, an.id_aeronave, an.tipo_aeronave,
+	an.capacidade
+FROM voo vo
+INNER JOIN aeronave an
+	ON vo.id_aeronave = an.id_aeronave
+ORDER BY vo.hora_partida;
 
--- Query 2: Obtém os detalhes dos bilhetes de voo, juntamente com as informações dos passageiros associados a esses bilhetes
-SELECT b.*, p.*
-FROM bilhete b
-INNER JOIN passageiro p ON b.id_passageiro = p.id_passageiro;
+/* 
+Query 2: Obtém os detalhes dos bilhetes de voo, juntamente com as informações dos passageiros associados a esses bilhetes
+Essa query é útil para obter todas as informações necessárias para um passageiro embarcar com seu bilhete, desde seus dados
+pessoais, quanto a origem e o destino deste.
+*/
+SELECT bi.numero_bilhete, bi.numero_voo, bi.numero_assento, bi.preco_bilhete, pa.id_passageiro,
+	vo.id_aeronave, vo.tipo_aeronave, vo.aeroporto_chegada, vo.hora_chegada, vo.aeroporto_partida, vo.hora_chegada,
+	pa.nome, pa.sobrenome, pa.endereco, pa.telefone, pa.email
+FROM bilhete bi
+INNER JOIN voo vo ON bi.numero_voo = vo.numero_voo
+INNER JOIN passageiro pa ON bi.id_passageiro = pa.id_passageiro
+ORDER BY vo.hora_partida;
 
 -- Query 3: Retorna as reservas feitas, incluindo os detalhes dos voos associados a essas reservas e os aeroportos de partida e chegada desses voos
 SELECT r.*, v.*, ap.nome_aeroporto AS aeroporto_partida, ac.nome_aeroporto AS aeroporto_chegada
@@ -15,14 +31,31 @@ INNER JOIN voo v ON r.numero_voo = v.numero_voo
 INNER JOIN aeroporto ap ON v.aeroporto_partida = ap.codigo_aeroporto
 INNER JOIN aeroporto ac ON v.aeroporto_chegada = ac.codigo_aeroporto;
 
--- Query 4: Retorna informações sobre os empregos dos funcionários, incluindo os detalhes dos aeroportos onde estão empregados
-SELECT e.*, f.*, a.*
-FROM emprego e
-INNER JOIN funcionario f ON e.id_funcionario = f.id_funcionario
-INNER JOIN aeroporto a ON e.codigo_aeroporto = a.codigo_aeroporto;
+/*
+Query 4: Retorna informações sobre os empregos dos funcionários, incluindo os detalhes dos aeroportos onde estão empregados
+Ordena prioritariamente os funcionários ativos (que não têm data de fim do emprego). Secundariamente, ordena os registros de
+emprego que forem mais antigos.
+*/
+SELECT em.id_emprego, em.data_inicio, em.data_fim,
+	em.id_funcionario, fu.cargo, fu.nome, fu.sobrenome, fu.salario,
+	em.codigo_aeroporto, ap.nome_aeroporto, ap.localizacao
+FROM emprego em
+INNER JOIN funcionario fu ON em.id_funcionario = fu.id_funcionario
+INNER JOIN aeroporto ap ON em.codigo_aeroporto = ap.codigo_aeroporto
+ORDER BY data_fim DESC, data_inicio ASC;
 
 -- Query 5: Obtém os detalhes dos voos, juntamente com as informações dos aeroportos de partida e chegada
 SELECT v.*, ap.nome_aeroporto AS aeroporto_partida, ac.nome_aeroporto AS aeroporto_chegada
 FROM voo v
 INNER JOIN aeroporto ap ON v.aeroporto_partida = ap.codigo_aeroporto
 INNER JOIN aeroporto ac ON v.aeroporto_chegada = ac.codigo_aeroporto;
+
+--- QUERIES AUXILIARES
+-- SELECT * FROM reserva;
+-- SELECT * FROM emprego;
+-- SELECT * FROM bilhete;
+-- SELECT * FROM voo;
+-- SELECT * FROM aeroporto;
+-- SELECT * FROM funcionario;
+-- SELECT * FROM passageiro;
+-- SELECT * FROM aeronave;
